611944905437b7bcd6f8c9f27f3f753070c0cf99
ALowton <ALowton@QA.com>
2022-03-09 15:44:25 +0000

    AL: added conditionals demo

--- src/com/qa/conditionals/IfStatements.java
+++
@@ -1,5 +0,0 @@
-package com.qa.conditionals;
-
-public class IfStatements {
-
-}
--- src/com/qa/conditionals/Runner.java
+++ src/com/qa/conditionals/Runner.java
@@ -3,8 +3,44 @@ package com.qa.conditionals;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num = 3;
+		int num2 = 5;
+		
+		// ONLY ONE of the conditions needs to be true
+		if (num == 3 || num2 == 10) {
+			System.out.println("Condition met"); // This is ran
+		} else { 
+			System.out.println("Condition not met");
+		}
+		
+		// BOTH of the conditions need to be true
+		if (num == 3 && num2 == 10) {
+			System.out.println("Condition met");
+		} else { 
+			System.out.println("Condition not met"); // This is ran
+		}
 
+		// Switch/Case
+		switch(num) {
+			case 0:
+				System.out.println("Number is 1");
+				break;
+			case 1:
+				System.out.println("Number is 2");
+				break;
+			case 2:
+				System.out.println("Number is 3");
+				break;
+			case 3:
+				System.out.println("Number is 3");
+				break;
+			case 4:
+				System.out.println("Number is 3");
+				break;
+			default:
+				System.out.println("Number is something else");
+				break;
+		}	
 	}
-
 }
